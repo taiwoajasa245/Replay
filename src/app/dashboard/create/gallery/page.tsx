e68dd9dc69e374f704df4007d639bca577adee5f
Ajasa Taiwo <ajasataiwo45@gmail.com>
2024-11-12 16:34:44 +0100

    initial commit

---
+++ src/app/dashboard/create/gallery/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CreateGallery() {
+  return (
+    <div>
+        <p> Create Gallery </p>
+    </div>
+  )
+}
--- src/components/Gallery/CreateGallaryButton.tsx
+++ src/components/Gallery/CreateGallaryButton.tsx
@@ -10,7 +10,7 @@ const CreateGalleryButton: React.FC<CreateGalleryButtonProps> = ({
   return (
     <button className="w-full flex justify-center">
       <Link
-        href="/dashboard"
+        href="/dashboard/create/gallery"
         className="px-12 py-2 md:px-10 md:py-2 rounded-[20px] text-[15px] md:text-[20px] text-white bg-[#305041] cursor-pointer transition duration-300 hover:bg-[#426d57]"
       >
         {text}
